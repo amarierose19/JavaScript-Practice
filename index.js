44d4dbebfbce0d8484dce82632499751c357b88e
alec <alec@alecs-MBP.lan>
2023-05-10 18:49:05 -0400

    Updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
   const name = 'john';
   const age = 30;
   
-  //Concatenation
+  //Concatenation addition (+ sign is the concatenation)
 console.log('My name is ' + name + ' and i am '+ age );
 
 //Template String 
@@ -18,21 +18,27 @@ const s = 'technology, computers, it, code';
 console.log(s.split(',') );
 
 
-// Arrays - variables that hold multiple values
+// ARRAYS - variables that hold multiple values
 
 const fruits = ['apples', 'oranges', 'pears'];
 
 fruits[3] = 'grapes';
+
 //add item to array
 fruits.push('mangos');
+
 // add item to the front of array
 fruits.unshift('strawberries');
+
 //removes last item from array
 fruits.pop();
+
 //check to see if there is an array or if it is an array
 console.log(Array.isArray(fruits))
+
 //Find index of certain items in array
 console.log(fruits.indexOf('oranges'))
+
 //calliing fruits
 console.log(fruits);
 
@@ -50,12 +56,15 @@ const person = {
 
 //how to access values 
 console.log(person.firstName, person.lastName);
+
 // how to get specific values from objects and arrays
 console.log(person.address.city); //or console.log(person.address[1])
+
 //how to create variables and pull out objects- destructoring 
 const {firstName, lastName, address: {city}} = person;
 
 console.log(firstName, lastName, city);
+
 // how to add new objects to array
 person.email = 'john@gmail.com';
 
@@ -78,18 +87,20 @@ const todos =  [
   iscompleted: true
 },
 ];
+
 //how to log specific objects within a variable
 console.log(todos[0].text);
+
 //how to convert into JSON within the script/ how to send data to a server
 const todoJSON = JSON.stringify(todos);
 console.log(todoJSON);
 
-// For Loops
+// FOR LOOPS
 for(let i = 0; i < 10; i++) {
   console.log(`For Loop number: ${i}`);
 }
 
-// While 
+// WHILE
 let i = 0;
 while(i < 10) {
   console.log(`While Loop Number: ${i}`);
@@ -112,7 +123,7 @@ todos.forEach(function(todo) {
 console.log(todo.text);
 });
 
-// Map allows you to create new arrays from existing arrays
+// Map allows you to create new arrays from existing arrays/ target each index of the array
 const todoText = todos.map(function(todo) {
   return todo.text;
   });
@@ -127,7 +138,7 @@ const todoCompleted = todos.filter(function(todo) {
 
 console.log(todoCompleted);
 
-//Condtionals
+//CONDITIONALS
 const x = 10;
 const y = 10;
 
@@ -151,3 +162,72 @@ if(x > 5 || y > 10) {
   console.log('x is NOT 10'); 
   //so if x changes from anything other than 10, it will show up not true
 }
+
+//terinary operator
+const a = 10;
+
+// '?' means then, ':' means else
+const color = a > 10 ? 'red' : 'blue';
+
+console.log(color);
+
+//switches- another way to right if, else if, and else conditionals 
+
+switch(color) {
+  case 'red';
+    console.log('color is red');
+    break;
+    case 'blue';
+      console.log('color is blue');
+      break;
+      default:
+        console.log('color is NOT or blue');
+        break;
+}
+
+//FUNCTION 
+function addNums(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addNums(5, 4)); //if there are no numbers in the parameters, it will show NaN (not a number) in the concsole log
+
+// Turn the about code into an arrow function
+const addNums = (num1 = 1, num2 = 1) => {
+  return num1 + num2;
+};
+
+console.log(addNums(5, 4));
+
+// or
+const addNums = (num1 = 1, num2 = 1) => num1 + num2;
+
+console.log(addNums(5, 4));
+
+// or 
+const addNums = num1 => num1 + 5;
+
+console.log(addNums(5));
+
+// example with .forEach 
+
+todos.forEach((todo) => console.log(todo));
+
+//OOP OBJECTED ORIENTED PROGRAMMING 
+
+// CONSTRUCTOR FUNCTION
+
+function Person(Namefirst, Namelast, dob) {
+  this.Namefirst = Namefirst;
+  this.Namelast = Namelast;
+  this.dob = dob;
+}
+
+// INSTANTIATE OBJECT 
+
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Smith', '3-6-1970');
+
+
+console.log(person1); // shows the string of new person made above
+console.log(person2);
